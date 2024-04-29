4ac9848392847e8c47d662de5224b14943e9ee67
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-04-29 20:14:05 +0530

    Add files via upload

---
+++ Elif-statement.py
@@ -0,0 +1,16 @@
+light = "yellow"
+
+if(light == "red"):
+    print("stop")
+ elif(light == "green"):
+    print("go")
+ elif(light == "yellow"):
+    print("look")
+ else: 
+     print("light is broken") 
+age = 14
+if(age >= 18):
+   print("can vote")
+ else:
+   print("cannot vote")  
+    
