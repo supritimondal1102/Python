5ff5df5e9e166f56fcf4fa8f2a19f370850b35b4
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-04-30 21:21:32 +0530

    Add files via upload

---
+++ Largest-number.py
@@ -0,0 +1,10 @@
+a = int(input("enter frist number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+
+if(a >= b and a >= c):
+    print("first nunber is largest", a)
+elif(b >=c):
+    print("second numberis largest", b)
+else:
+    print("third is largest", c)        
---
+++ Odd-Evennumber.py
@@ -0,0 +1,6 @@
+num = int(input("enter number: "))
+rem = num % 2
+if(rem == 0):
+    print("EVEN")
+else:
+    print("ODD")    
