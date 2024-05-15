72f1db74e2f3f40071d171a2b88a9883528c366e
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-05-15 21:59:56 +0530

    Add files via upload

---
+++ Loops qs2.py
@@ -0,0 +1,12 @@
+nums = (1,4,9,6,56,90,78,56,100)
+
+x = 90
+
+i = 0
+while i< len (nums):
+    if(nums[i] == x):
+        print("FOUND a idx", i)
+    else:
+        print("finding..")    
+    i += 1
+
